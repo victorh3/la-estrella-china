fc981daf186309ead9f0c1f03494f6fc09e695f9
victorh3 <dvictorhu@gmail.com>
2025-07-03 00:15:09 -0500

    Delete unused code

--- src/MarketingPage.tsx
+++ src/MarketingPage.tsx
@@ -3,20 +3,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
-declare module "@mui/material/styles" {
-  interface Theme {
-    status: {
-      danger: string;
-    };
-  }
-  // allow configuration using `createTheme()`
-  interface ThemeOptions {
-    status?: {
-      danger?: string;
-    };
-  }
-}
-
 const theme = createTheme({
   palette: {
     background: {
